7fdedcdedb925ae37aa675eb3da225815f197b43
Meaghan Kish <106093711+Mkish1220@users.noreply.github.com>
2022-11-20 11:43:51 -0500

    added links to navbar
    home, products, nursery, about us, contact pages link

--- unclepete-react-app/src/App.js
+++ unclepete-react-app/src/App.js
@@ -1,10 +1,20 @@
+// import Product from "./components/About";
+// import Contact from "./components/Contact";
+// import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+// import Nursery from "./components/Skills";
+// import AboutUs from "./components/Work";
+
 
 function App() {
   return (
     <div>
-         <Navbar />
-
+      <Navbar />
+      {/* <Home /> */}
+      {/* <Product /> */}
+      {/* <Nursery /> */}
+      {/* <AboutUs /> */}
+      {/* <Contact /> */}
     </div>
   );
 }
--- unclepete-react-app/src/components/Navbar.jsx
+++ unclepete-react-app/src/components/Navbar.jsx
@@ -1,13 +1,50 @@
 import React from 'react'
+
+import { Link } from 'react-scroll';
 import Logo from '../assets/logo.png';
 
+
 const Navbar = () => {
+ 
+  
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+        <img src={Logo} alt='Logo Image' style={{ width: '200px' }} />
+      </div>
 
-      <img src={Logo} alt='UPP Logo' style={{ width: '200px' }} />
-      
-    </div>
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='product' smooth={true} duration={500}>
+            Product
+          </Link>
+        </li>
+        <li>
+          <Link to='nursery' smooth={true} duration={500}>
+            Nursery
+          </Link>
+        </li>
+        <li>
+          <Link to='aboutus' smooth={true} duration={500}>
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      </div>
+
   );
 };
 
